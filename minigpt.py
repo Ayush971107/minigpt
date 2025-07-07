2aa06a2c7286a4e5768a71b052c510fce2dc519c
Ayush Roy <ayush.roy9711@gmail.com>
2025-07-07 16:35:45 -0700

    c4

--- minigpt.py
+++ minigpt.py
@@ -1,4 +1,7 @@
 import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 with open('gutenberg-poetry-v001.ndjson', encoding='utf-8') as fin, \
      open('poems.txt', 'w', encoding='utf-8') as fout:
@@ -21,3 +24,35 @@ def encode (chars):
 
 def decode (indices):
   return "".join([itos[i] for i in indices])
+
+
+# Train and test splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
