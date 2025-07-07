d8af0fef24abad4bf392a4cf971ffc5a52387d9b
Ayush Roy <ayush.roy9711@gmail.com>
2025-07-07 16:37:53 -0700

    c8

--- minigpt.py
+++ minigpt.py
@@ -109,3 +109,15 @@ class MultiHeadAttention(nn.Module):
     out = self.proj(out)
     out = self.dropout(out)
     return out
+
+class FeedFoward(nn.Module):
+  def __init__(self,n_embd):
+    super().__init__()
+    self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+  def forward(self,x):
+    return self.net(x)
