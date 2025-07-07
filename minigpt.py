75fca75d33d25b45d90f7d8477f0f8a5eebd6720
Ayush Roy <ayush.roy9711@gmail.com>
2025-07-07 16:37:21 -0700

    c6

--- minigpt.py
+++ minigpt.py
@@ -95,3 +95,17 @@ class Head(nn.Module):
         v = self.value(x)
         out = wei @ v # gives us B,T,C again
         return out
+
+class MultiHeadAttention(nn.Module):
+  def __init__(self, num_heads, head_size):
+    super().__init__()
+    # we create num_heads heads and run them in parallel
+    self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    self.proj = nn.Linear(n_embd,n_embd)
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self,x):
+    out = torch.cat([h(x) for h in self.heads],dim = -1)
+    out = self.proj(out)
+    out = self.dropout(out)
+    return out
